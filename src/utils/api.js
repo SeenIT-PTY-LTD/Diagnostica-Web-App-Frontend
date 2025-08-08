459af3b502b77eb9793cc1dd02433f6b65b8c8cc
kunalBari5557 <kunal@humb.io>
2025-08-08 12:56:31 +0530

    update baseurl for test env

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://192.168.0.104:3003",
-  // baseURL: "https://d2l873mxalz3b9.cloudfront.net",
-  baseURL: "http://localhost:3003",
+  baseURL: "https://d2l873mxalz3b9.cloudfront.net",
+  // baseURL: "http://localhost:3003",
 });
 
 // Add request interceptor to inject token
